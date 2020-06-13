92f5f2aab39e9140fc8bc1bd348c04afc38730ac
Moritz Freyburger <moritz.freyburger@gmx.de>
2020-06-13 16:18:16 +0200

    add some more samples

---
+++ system-linq-async/MultipleTasksParallel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace system_linq_async
+{
+    // Get a lot of work done the old fashioned way
+    class MultipleTasksParallel
+    {
+        static async Task DoAsync()
+        {
+            EnumerateMultiple().AsParallel()
+                               .WithDegreeOfParallelism(4)
+                               .ForAll(async task =>
+            {
+                var number = await task;
+
+                // Do something after it has finished;
+            });
+
+            Console.WriteLine("All Done!");
+        }
+
+        static IEnumerable<Task<int>> EnumerateMultiple()
+        {
+            yield return Task.Run(() => 0);
+            yield return Task.Run(() => 1);
+            yield return Task.Run(() => 2);
+        }
+    }
+}
---
+++ system-linq-async/SelectAwait.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace system_linq_async
+{
+    // Use Select await for asynchronous select
+    class SelectAwait
+    {
+        static async Task DoAsync()
+        {
+            // As lambda
+            var selected = EnumerateAsync().SelectAwait(async number =>
+            {
+                await Task.Delay(1);
+                return number + 1;
+            });
+
+            await foreach (var number in selected)
+            {
+                // ...
+            }
+
+            // As method group
+            EnumerateAsync().SelectAwait(ModifyDelayed);
+        }
+
+        static async IAsyncEnumerable<int> EnumerateAsync()
+        {
+            yield return 0;
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            yield return 1;
+        }
+
+        static async ValueTask<int> ModifyDelayed(int number)
+        {
+            await Task.Delay(1);
+
+            return number + 1;
+        }
+    }
+}
