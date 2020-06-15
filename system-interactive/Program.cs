1fafba94e24082dc069f39d7d06fee38ea88a9bc
Moritz Freyburger <moritz.freyburger@gmx.de>
2020-06-15 19:45:50 +0200

    Add reactive and interactive

---
+++ system-interactive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace system_interactive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ system-reactive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace system_reactive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
