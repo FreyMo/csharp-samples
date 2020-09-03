12c97adce39c5d8f0f78f0d83d34772f63ccd577
Moritz Freyburger <moritz.freyburger@gmx.de>
2020-09-03 22:16:41 +0200

    Add memory, span, slicing and ranges

---
+++ span-of-t/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace span_of_t
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SliceIt("");
+        }
+    }
+
+    public static void DoStuff()
+    {
+
+    }
+
+    public ReadOnlySpan<char> SliceIt(ReadOnlySpan<char> span)
+    {
+        var temp = span.Overlaps("");
+
+    }
+}
