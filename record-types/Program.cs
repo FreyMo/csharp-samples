ece9d4a6e6cb95d0ef0d139c1143e81310c374a7
Moritz Freyburger <moritz.freyburger@gmx.de>
2020-09-03 17:07:50 +0200

    Add record types

---
+++ record-types/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace record_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rec = new MyRecord
+            {
+                MyInt = 3,
+                MyString = ""
+            };
+
+            var anotherRecord = rec with { MyInt = 4 };
+            // rec.MyInt = 4;
+
+            Console.WriteLine(rec.MyInt);
+            Console.WriteLine(anotherRecord.MyInt);
+            Console.WriteLine(anotherRecord == rec);
+        }
+    }
+
+
+
+
+
+
+
+    public record MyRecord
+    {
+        public int MyInt { get; init; }
+        public string MyString { get; init; }
+    }
+}
